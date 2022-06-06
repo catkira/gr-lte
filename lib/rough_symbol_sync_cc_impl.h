f84453fd0587303521370e1f4ec6cfdcc3c85400
Benjamin Menküc <benjamin.menkuec@fh-dortmund.de>
2022-06-06 19:47:07 +0200

    added rough_symbol_sync_cc

---
+++ lib/rough_symbol_sync_cc_impl.h
@@ -0,0 +1,48 @@
+#ifndef INCLUDED_LTE_ROUGH_SYMBOL_SYNC_CC_IMPL_H
+#define INCLUDED_LTE_ROUGH_SYMBOL_SYNC_CC_IMPL_H
+
+#include <gnuradio/lte/rough_symbol_sync_cc.h>
+
+namespace gr {
+  namespace lte {
+
+    class rough_symbol_sync_cc_impl : public rough_symbol_sync_cc
+    {
+     private:
+        int d_fftl;
+        int d_cpl;
+        int d_cpl0;
+        int d_slotl;
+        long d_sym_pos;
+        float d_corr_val;
+        int d_work_call;
+        int d_vlen;
+        gr_complex* d_cp0;
+        gr_complex* d_cp1;
+        gr_complex* d_res;
+        
+        pmt::pmt_t d_key;
+        pmt::pmt_t d_tag_id;
+        
+        gr_complex corr(gr_complex *x,gr_complex *y, int len);
+        gr_complex corr(gr_complex *res, gr_complex *x, gr_complex *y, int len);
+        
+     public:
+      rough_symbol_sync_cc_impl(int fftl, int vlen, std::string name);
+      ~rough_symbol_sync_cc_impl();
+      
+      void forecast(int noutput_items, 
+	       gr_vector_int &ninput_items_required);      
+
+      // Where all the action really happens
+      int work(
+              int noutput_items,
+              gr_vector_const_void_star &input_items,
+              gr_vector_void_star &output_items
+      );
+    };
+
+  } // namespace lte
+} // namespace gr
+
+#endif /* INCLUDED_LTE_ROUGH_SYMBOL_SYNC_CC_IMPL_H */
--- python/lte/bindings/python_bindings.cc
+++ python/lte/bindings/python_bindings.cc
@@ -30,6 +30,7 @@ namespace py = pybind11;
     void bind_sss_symbol_selector_cvc(py::module& m);
     void bind_sync_frequency_c(py::module& m);
     void bind_subblock_deinterleaver_vfvf(py::module& m);
+    void bind_rough_symbol_sync_cc(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
 
@@ -67,5 +68,6 @@ PYBIND11_MODULE(lte_python, m)
     bind_sss_symbol_selector_cvc(m);
     bind_sync_frequency_c(m);
     bind_subblock_deinterleaver_vfvf(m);
+    bind_rough_symbol_sync_cc(m);
     // ) END BINDING_FUNCTION_CALLS
 }
